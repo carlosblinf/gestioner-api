ed6862134af4da94d48dd40ffaaf1db2a2bb1f8f
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-05 15:49:20 +0200

    model Persona filled

--- app/Persona.php
+++ app/Persona.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
-    //
+	const PERSONA_MEMBER = 'true';
+	const PERSONA_NO_MEMBER = 'false';
+
+    protected $fillable = [
+        'name', 'lastname','ci', 'address', 'municipality', 'province', 'gender',
+        'phone', 'celphone', 'email', 'civil_status', 'date_birth', 'ocupations',
+        'professions', 'desease', 'celula', 'member', 'structure_id', 'activity_id',
+    ];
+
+    public function isMember(){
+    	return $this->member == Persona::PERSONA_MEMBER;
+    }
 }
