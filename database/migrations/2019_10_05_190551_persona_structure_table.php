14ae7899bf1a382d47574ffe7ec7e4541570a895
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-05 21:09:25 +0200

    persona_structure migration for relations

---
+++ database/migrations/2019_10_05_190551_persona_structure_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PersonaStructureTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persona_structure', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('persona_id');
+            $table->unsignedInteger('structure_id');
+            $table->foreign('persona_id')->references('id')->on('personas');
+            $table->foreign('structure_id')->references('id')->on('structure');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persona_structure');
+    }
+}
