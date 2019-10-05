0e865804ff402171e7866fddbd6b6b39f176eae5
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-05 20:30:03 +0200

    user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,9 +17,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nickname')->unique();
             $table->string('password');
+            $table->string('admin')->default(User::REGULAR_USER);
+            $table->string('actived')->default(User::USER_NOT_ACTIVED);
             $table->rememberToken();
             $table->timestamps();
         });
