049fe25f1fae4d156a0963580b97af973a83f033
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-05 19:21:04 +0200

    model Structure filled

--- app/Structure.php
+++ app/Structure.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use App\Persona;
 use Illuminate\Database\Eloquent\Model;
 
 class Structure extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description', 'chief', 'persona_id', 'activity_id',
+    ];
 }
