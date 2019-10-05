de71f3acd9c5b29042eaf5a146b8c647b1700c8b
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-05 21:12:07 +0200

    activity_structure migration for relations

---
+++ database/migrations/2019_10_05_191048_activity_structure_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ActivityStructureTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_structure', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('activity_id');
+            $table->unsignedInteger('structure_id');
+            $table->foreign('activity_id')->references('id')->on('activities');
+            $table->foreign('structure_id')->references('id')->on('structure');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_structure');
+    }
+}
