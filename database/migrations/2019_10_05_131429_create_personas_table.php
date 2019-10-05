116f5d6ef139b3cc855b91e016308252f3a0e375
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-05 20:34:18 +0200

    personas migration

--- database/migrations/2019_10_05_131429_create_personas_table.php
+++ database/migrations/2019_10_05_131429_create_personas_table.php
@@ -15,6 +15,23 @@ class CreatePersonasTable extends Migration
     {
         Schema::create('personas', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name'); 
+            $table->string('lastname');
+            $table->unsignedInteger('ci');
+            $table->string('address');
+            $table->string('municipality');
+            $table->string('province');
+            $table->string('gender');
+            $table->string('phone')->nullable();
+            $table->string('celphone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('civil_status');
+            $table->string('date_birth');
+            $table->string('ocupations')->nullable();
+            $table->string('professions')->nullable();
+            $table->string('desease')->nullable();
+            $table->string('celula')->nullable();
+            $table->string('member')->default(Persona::PERSONA_NO_MEMBER);
             $table->timestamps();
         });
     }
