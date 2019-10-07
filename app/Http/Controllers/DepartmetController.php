bd34afe9f835f34928e760bb92d6ef80a6a5b6c9
Carlos Ernesto <carlosblinf@gmail.com>
2019-10-07 14:12:41 +0200

    model, controll Department added

---
+++ app/Http/Controllers/DepartmetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Departmet;
+use Illuminate\Http\Request;
+
+class DepartmetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Departmet  $departmet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Departmet $departmet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Departmet  $departmet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Departmet $departmet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Departmet  $departmet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Departmet $departmet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Departmet  $departmet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Departmet $departmet)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_10_07_121000_create_departmets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departmets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departmets');
+    }
+}
